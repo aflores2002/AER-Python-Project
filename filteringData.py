f094be4069dbc77a0859c7a032380632a7ca9d78
Alfredo Flores <a3floresjr@gmail.com>
2023-10-31 13:11:43 -0700

    Delete filteringData.py

--- filteringData.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-#skips first 13 rows
-df = pd.read_csv("Charlie-DataLog-9-24.csv", skiprows=13,
-                 usecols=['Time', 'D1 DC Bus Voltage','D4 DC Bus Current','D4 VBC Vq Voltage',
-                          'D2 Output Voltage','D3 HV Input Current','D2 Max Charge Current','Fast DC Bus Voltage'])
-print(df)
